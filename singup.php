74ff6c020f71d5acc0bc98c9cb336549e1a172b1
andresdgv98 <68084624+andresdgv98@users.noreply.github.com>
2022-11-14 10:25:45 -0400

    Update singup.php

--- singup.php
+++ singup.php
@@ -6,7 +6,7 @@
     
     <div class="login-box">
       <img src="assets/imagenes/logo2.png" class="avatar" alt="Avatar Image">
-      <h1>Ingreso</h1>
+      <h1>Registro</h1>
       <form action="registro.php" method="POST">
         <!-- NAME INPUT -->
         <label for="nombre">Nombre y apellido</label>
